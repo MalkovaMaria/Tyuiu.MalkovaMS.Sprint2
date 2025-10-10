71a11d030a07b0bab6bab16de45af4c0189a8438
Мария Малькова <Tyuiu.MalkovaMS@outlook.com>
2025-10-10 19:48:14 +0500

    Task3

---
+++ Tyuiu.MalkovaMS.Sprint2.Task3.V30/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.MalkovaMS.Sprint2.Task3.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        double x;
+        Console.Title = "Спринт #2 | Выполнила: Малькова М. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #30                                                             *");
+        Console.WriteLine("* Выполнила: Малькова Мария Сергеевна | ИИПб-25-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции У с    *");
+        Console.WriteLine("* использованием вложенных операторов if-elseу                            *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите значение Х:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double resultat = ds.Calculate(x);
+        Console.WriteLine("Значение функции = " + resultat);
+
+        Console.ReadKey();
+    }
+}
