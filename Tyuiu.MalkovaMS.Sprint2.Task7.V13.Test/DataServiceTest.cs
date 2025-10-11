58139e650aeddc01981ba4765f9ad1c845a7c4b7
Мария Малькова <Tyuiu.MalkovaMS@outlook.com>
2025-10-11 23:29:20 +0500

    Task7

---
+++ Tyuiu.MalkovaMS.Sprint2.Task7.V13.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MalkovaMS.Sprint2.Task7.V13.Lib;
+
+namespace Tyuiu.MalkovaMS.Sprint2.Task7.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = 0.2;
+            double y = -0.5;
+            Assert.AreEqual(true, ds.CheckDotInShadedArea(x, y));
+        }
+    }
+}
---
+++ Tyuiu.MalkovaMS.Sprint2.Task7.V13/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.MalkovaMS.Sprint2.Task7.V13.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        double x, y;
+        Console.Title = "Спринт #2 | Выполнила: Малькова М. С. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #13                                                             *");
+        Console.WriteLine("* Выполнила: Малькова Мария Сергеевна | ИИПб-25-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает вещественные значения и         *");
+        Console.WriteLine("* вычисляет, находится ли точка с координатами Х,У в заштрихованной обл.  *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите значение Х:");
+        x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение У:");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        bool res = ds.CheckDotInShadedArea(x, y);
+        if (res == true)
+            Console.WriteLine("Точка находится в заштрихованной области");
+        else
+            Console.WriteLine("Точка не находится в заштрихованной области");
+        Console.ReadKey();
+    }
+}
